e268ee9bb0d6028a71ff3ba55ad78ca85feb5c62
Upasana9 <72407393+Upasana9@users.noreply.github.com>
2020-10-25 16:30:56 +0530

    Kadane's_Algorithm.c

---
+++ Kadane's_Algorithm.c
@@ -0,0 +1,22 @@
+#include<stdio.h>using namespace std;
+int main()
+{int t;
+ scanf("%d",&t);
+ while(t--)
+ {int n;
+ scanf("%d",&n);
+ int a[n];
+ int i;
+ for(i=0;i<n;i++)
+  scanf("%d",&arr[i]);
+  int sum=0;int ans=INT_MIN;
+  for(i=0;i<n;i++)
+   {sum+=a[i];ans=max(ans,sum);
+   if(sum<0)
+     sum=0;}
+     printf("%d",ans);}}
+   
+   
+ 
+    
+    
