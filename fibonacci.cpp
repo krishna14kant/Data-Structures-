e8a7e9f76f2b47d57f28edb7cc1907f6f44bfb45
Agrasth <64785911+agrasthnaman@users.noreply.github.com>
2021-10-03 23:42:42 +0530

    optimized to work in O(Logn) for fibonacci
    Please Merge the optimized code for fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,26 +1,53 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-int Fib(int n)
+ 
+void multiply(int F[2][2], int M[2][2]);
+void power(int F[2][2], int n);
+ 
+// Function that returns nth Fibonacci number
+int fib(int n)
 {
-    if (n == 0 || n == 1)
-    {
-        return n;
-    }
-    else {
-        return Fib(n-1) + Fib(n-2);
-    }
+    int F[2][2] = {{1, 1}, {1, 0}};
+    if (n == 0)
+        return 0;
+    power(F, n - 1);
+ 
+    return F[0][0];
+}
+ 
+// Optimized version of power() in method 4
+void power(int F[2][2], int n)
+{
+    if(n == 0 || n == 1)
+       return;
+    int M[2][2] = {{1, 1}, {1, 0}};
+     
+    power(F, n / 2);
+    multiply(F, F);
+     
+    if (n % 2 != 0)
+        multiply(F, M);
+}
+ 
+void multiply(int F[2][2], int M[2][2])
+{
+    int x = F[0][0] * M[0][0] + F[0][1] * M[1][0];
+    int y = F[0][0] * M[0][1] + F[0][1] * M[1][1];
+    int z = F[1][0] * M[0][0] + F[1][1] * M[1][0];
+    int w = F[1][0] * M[0][1] + F[1][1] * M[1][1];
+     
+    F[0][0] = x;
+    F[0][1] = y;
+    F[1][0] = z;
+    F[1][1] = w;
 }
 
-int main(){
-    int n,j=0;
-    cout << "Enter the total number of terms:";
-    cin >> n;
-
-    for(int i=1; i<=n; i++)
-    {
-            int r = Fib(j);
-            cout << r << " ";
-            j++;
-    }
+int main()
+{
+    int n = 9;
+     
+    cout << fib(9);
+    getchar();
+     
+    return 0;
 }
