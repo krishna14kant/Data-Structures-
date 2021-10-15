c10829669ba1d51f8fc4441b619f42b2f2391d7a
Anova07 <65520755+Anova07@users.noreply.github.com>
2021-10-15 11:39:46 +0530

    Create CheckDuplicates.cpp

---
+++ C++/CheckDuplicates.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  
+public:
+  
+    bool containsDuplicate(vector<int>& nums) {
+      
+        sort(nums.begin(), nums.end());
+      
+        for(int i = 1; i < nums.size(); i++)
+        {
+          
+            if(nums[i-1] == nums[i])
+                return true;
+          
+        }
+      
+        return false;
+    }
+  
+};
