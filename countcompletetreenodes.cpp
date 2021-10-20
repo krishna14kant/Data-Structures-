9dd6eaeaaeb3bde0ab2279f1ae6605a608065f4f
Pratu07-prog <vpratiksha.chandrakant.mec20@itbhu.ac.in>
2021-10-20 14:59:40 +0530

    first PR

--- countcompletetreenodes.cpp
+++ countcompletetreenodes.cpp
@@ -1,12 +1,19 @@
- struct TreeNode 
- {
+ 
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+struct TreeNode 
+{
     int val;
     TreeNode *left;
     TreeNode *right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
+};
  
 class Solution {
 public:
