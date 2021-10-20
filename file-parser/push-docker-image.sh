4fb98376397e1adca32c6343da998ebeec573745
Fernando Merino <fermervil@gmail.com>
2021-10-20 19:02:08 +0200

    Add Node csv files parser

---
+++ file-parser/push-docker-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo ""
+echo "1.- ELIMINANDO CONTENEDOR E IMAGEN"
+docker stop file-parser || echo "El contenedor file-parser no está ejecutándose"
+docker container rm file-parser || echo "El contenedor file-parser no existe"
+docker image rm ferbosonit/file-parser:latest || echo "La imagen ferbosonit/file-parser:latest no existe"
+echo ""
+echo "2.- CONSTRUYENDO CONTENEDOR"
+docker build -t ferbosonit/file-parser:latest .
+echo ""
+echo "3.- SUBIENDO IMAGEN A DOCKERHUB"
+docker push ferbosonit/file-parser:latest
