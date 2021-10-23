a3e33d4d9046c8cb5feb82a55083da8d16bd74cc
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2021-10-23 20:22:36 +0530

    Create RCBPLAY.cpp

---
+++ Codechef/RCBPLAY.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	// code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    if(y-x<=z*2){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	    
+	    
+	}
+	return 0;
+}
