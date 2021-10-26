6062a32027a9160011c3d10acb72c8a60af3efa2
rohanlahori <64496391+rohanlahori@users.noreply.github.com>
2021-10-26 13:43:37 +0530

    Create 1602A.cpp

---
+++ Codeforces/1602A.cpp
@@ -0,0 +1,159 @@
+Problem Link:
+https://codeforces.com/contest/1602/problem/A
+
+My Submission Link:
+https://codeforces.com/contest/1602/submission/132986949
+
+Code:
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#include <iostream>
+#include <bits/stdc++.h> 
+#include <algorithm>
+using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+#define ll long long 
+#define all(x) (x).begin(),(x).end()
+#define vll vector<ll>
+#define vi vector<ll>
+#define vp vector<pi>
+#define vs vector<string>
+#define pb push_back
+#define mp make_pair
+#define POB pop_back 
+#define f(i,x,z) for(ll i=x;i<z;i++)
+#define debug(x) cout<<'DEBUG------> '<<x<<endl
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define clr(x) memset(x, 0, sizeof(x)) 
+#define setall(x,a) memset(x, a, sizeof(x))
+#define INF 1000000000000000003 
+#define mathpi 3.141592653589793238 
+#define F first;
+#define S second;
+#define vpair vector<pair<ll,ll>>
+#define vpairs vector<pair<string,string>>
+#define tin tuple<ll,ll,ll>
+#define rev(i,x,z) for(ll i=x;i>=z;i--)  
+#define ld long double 
+#define print(v) for(auto x:v){cout<<x<<" ";}cout<<"\n"; 
+#define print_pair(v) for(auto x:v){cout<<x.first<<" "<<x.second<<" ";}cout<<"\n"; 
+#define reverse(v) reverse(v.begin(),v.end()) 
+#define nl "\n"
+vi v;
+void sieve(ll n)
+{
+    vector<char> is_prime(n+1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i <= n; i++) 
+    {
+        if (is_prime[i] && (long long)i * i <= n) 
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                is_prime[j] = false;
+            }
+        }
+    }
+    f(i,0,n)
+    {
+        if(is_prime[i]==1)
+        {
+            v.pb(i);
+        }
+    }
+}
+ll binaryexponention(ll a,ll b)
+{
+	ll result=1,md=1000000007;
+	while(b>0)
+	{
+		if(b%2==1)
+		{
+			result*=a;
+		}
+		a*=a;
+		b/=2;
+	}
+	return result;
+}
+
+void cf_round()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b,c;
+        cin>>a>>b>>c;
+        ll x=0,y=0;
+        x+=c/2;
+        y+=c/2;
+        if(c%2!=0)
+        {
+            x+=3;
+        }
+        if(x>y)
+        {
+            if(b>0)
+            {
+                if(a%2!=0)
+                {
+                    cout<<0<<nl;
+                }
+                else{
+                    cout<<1<<nl;
+                }
+            }
+            else{
+                if(a%2==0)
+                {
+                    cout<<1<<nl;
+                }
+                else{
+                    cout<<0<<nl;
+                }
+            }
+        }
+        else{
+
+        }
+    }
+}
+int main()
+{
+    // freopen("inp.txt","r",stdin);
+    // freopen("out.txt","w",stdout);
+    ios::sync_with_stdio(0);// Input and output clearance
+    cin.tie(0);
+    // cf_round();   
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+       string s;
+       cin>>s;
+       string s1=s;
+       sort(s.begin(),s.end());
+       cout<<s[0]<<" ";
+       bool x=0;
+       f(i,0,s1.length())
+       {
+           if(s1[i]!=s[0] || x==1)
+           {
+                cout<<s1[i];
+           }
+           if(s1[i]==s[0])
+           {
+               x=1;
+           }
+       }
+       cout<<nl;
+    }
+
+}
+//  think brutforce again
+// make templates shortcut small
