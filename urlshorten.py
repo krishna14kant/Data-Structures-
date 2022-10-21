f2ce15d5dc8b799fb503b0d8f51c861b24a1cdd6
Prathamesh Walawalkar <51331118+RenmoTC@users.noreply.github.com>
2022-10-21 23:56:32 +0530

    Create urlshorten.py

---
+++ urlshorten.py
@@ -0,0 +1,20 @@
+# shortener_app/main.py
+
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+    db_url = models.URL(
+        target_url=url.target_url, key=key, secret_key=secret_key
+    )
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    db_url.url = key
+    db_url.admin_url = secret_key
+
+    return db_url
