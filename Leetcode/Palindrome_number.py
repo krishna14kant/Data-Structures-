47a583287754d273bc7958db5ce13e0ff77faebc
GauravRai2002 <88336754+GauravRai2002@users.noreply.github.com>
2022-10-24 20:19:32 +0530

    matrix Chain Multiplication solution in Java

--- Leetcode/Palindrome_number.py
+++
@@ -1,7 +0,0 @@
-# Palindrome Number
-x=int(input())
-x=str(x)
-if x==x[::-1]:
-    return True #Palindrome
-else:
-    return False #Not Palindrome
--- areaoftriangle.py
+++
@@ -1,7 +0,0 @@
-a = float(input('1 side: '))  
-b = float(input('2 side: '))  
-c = float(input('3 side: '))  
-  
-s = (a + b + c) / 2  
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5  
-print('The area of the triangle is %0.4f' %area)   
