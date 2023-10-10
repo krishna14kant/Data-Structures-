caa2053b45f16278f11ce97889aa57544ea1f94c
Jyoti Pandey <91590380+jyotip31@users.noreply.github.com>
2023-10-10 21:31:23 +0530

    Update 1._Two_Sum.cpp
    1. Added comments to explain the code logic.
    2. Used a consistent naming convention and improved variable names for better readability.
    3. Moved #include statements to the top of the code for clarity.
    4. Adjusted indentation and formatting to follow a standard coding style.

--- Leetcode/1._Two_Sum.cpp
+++ Leetcode/1._Two_Sum.cpp
@@ -1,18 +1,31 @@
-//1. Two Sum Leetcode Solution
+// Two Sum LeetCode Solution
+
+#include <vector>
+#include <unordered_map>
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-     unordered_map<int,int> hash;
-     vector<int> ans;
-        for(int i = 0;i<nums.size();i++)
-            if(hash.count(target-nums[i])>0)
-            {
-                ans.push_back(hash[target-nums[i]]);
-                ans.push_back(i);
-                return ans;
-            }   
-            else
-                hash[nums[i]] = i;
-        return ans;
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> hash_map;
+        std::vector<int> result;
+
+        // Iterate through the array
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            // Check if the complement is in the hash map
+            auto it = hash_map.find(complement);
+            if (it != hash_map.end()) {
+                // Found a pair that sums to the target
+                result.push_back(it->second);
+                result.push_back(i);
+                return result;
+            }
+
+            // Store the current number and its index in the hash map
+            hash_map[nums[i]] = i;
+        }
+
+        return result; // Return an empty vector if no solution is found
     }
 };
